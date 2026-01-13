5fae9d417f8226e2826922d5d64d1160de3be9c3
Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>
2026-01-13 14:30:31 +0000

    ASoC: codecs: hdmi-codec: do not attempt to setup while disconnected
    There is no point in trying to prepare the codec when the cable is
    disconnected.
    This can help in some situations with pipewire which will try to prepare
    the dai-link even when the jack is disconnected.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>

--- sound/soc/codecs/hdmi-codec.c
+++ sound/soc/codecs/hdmi-codec.c
@@ -452,6 +452,9 @@ static int hdmi_codec_startup(struct snd_pcm_substream *substream,
 	if (!((has_playback && tx) || (has_capture && !tx)))
 		return 0;
 
+	if (!hcp->jack_status)
+		return 0;
+
 	mutex_lock(&hcp->lock);
 	if (hcp->busy) {
 		dev_err(dai->dev, "Only one simultaneous stream supported!\n");
@@ -575,6 +578,9 @@ static int hdmi_codec_hw_params(struct snd_pcm_substream *substream,
 	};
 	int ret;
 
+	if (!hcp->jack_status)
+		return 0;
+
 	if (!hcp->hcd.ops->hw_params)
 		return 0;
 
@@ -615,6 +621,9 @@ static int hdmi_codec_prepare(struct snd_pcm_substream *substream,
 	unsigned int rate = runtime->rate;
 	struct hdmi_codec_params hp;
 	int ret;
+
+	if (!hcp->jack_status)
+		return 0;
 
 	if (!hcp->hcd.ops->prepare)
 		return 0;
@@ -717,6 +726,9 @@ static int hdmi_codec_mute(struct snd_soc_dai *dai, int mute, int direction)
 	 * see
 	 *	snd_soc_dai_digital_mute()
 	 */
+	if (!hcp->jack_status)
+		return 0;
+
 	if (hcp->hcd.ops->mute_stream &&
 	    (direction == SNDRV_PCM_STREAM_PLAYBACK ||
 	     !hcp->hcd.no_capture_mute))
