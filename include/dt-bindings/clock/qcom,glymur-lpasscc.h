febb04406c1505a10a0423ab746f88b7c918bd14
Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>
2026-09-21 13:15:22 +0100

    dt-bindings: clock: Add LPASS AUDIOCC and reset controller for Glymur
    The LPASS (Low Power Audio Subsystem) Audio clock controller provides reset
    support when it is under the control of Q6DSP.
    
    Add support for those resets and adds IDs for clients to request the reset.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>

---
+++ include/dt-bindings/clock/qcom,glymur-lpasscc.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_LPASSCC_GLYMUR_H
+#define _DT_BINDINGS_CLK_QCOM_LPASSCC_GLYMUR_H
+
+/* LPASS AUDIO CC CSR */
+#define LPASS_AUDIO_SWR_RX_CGCR				0
+#define LPASS_AUDIO_SWR_WSA1_CGCR			1
+#define LPASS_AUDIO_SWR_WSA2_CGCR			2
+#define LPASS_AUDIO_SWR_WSA3_CGCR			3
+#define LPASS_AUDIO_SWR_WSA4_CGCR			4
+
+#endif
